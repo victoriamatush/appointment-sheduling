83ea6d6c3fc4ebdd1084908d14da083b0ecb7812
victoria <victoria603111@gmail.com>
2022-04-26 14:30:21 +0300

    This is a new commit

---
+++ AppointmentSheduling/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppointmentSheduling.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
